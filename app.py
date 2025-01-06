5a7ef8a06fc6d21f8f2b64371e5ed6f1422bae30
youdiscoveredmadz <maddieskinkis@gmail.com>
2025-01-05 23:47:05 -0500

    I GOT IT TO WORK!!

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
 from flask import Flask
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db=SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.name
 
 
+user=User(username='john', email='john@example.com')
+db.session.add(user)
+db.session.commit()
 
+users=User.query.all()
+
+@app.route('/')
+def index():
+    return render_template('index.html', users=users)
 
